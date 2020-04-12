61c903ad5b2da5f64aa9dcfa28320b55b10c2d10
slimmind <slimmind@gmail.com>
2020-04-12 16:44:49 +0300

    workking on toggle-forms

--- notix/src/js/modules/toggle-item-list.js
+++ notix/src/js/modules/toggle-item-list.js
@@ -1,7 +1,7 @@
-export default function toggleItemList(DOM) {
-    for(const input of DOM.listTypeInput) {
+export default function toggleItemList() {
+    for(const input of vars.DOM.listTypeInput) {
         input.addEventListener('change', (event) => {
-            for(const list of DOM.itemsList) {
+            for(const list of vars.DOM.itemsList) {
                 list.classList.add('hidden');
             }
             document.querySelector(`.${event.target.value}`).classList.remove('hidden');
