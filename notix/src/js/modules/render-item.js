071df3c091b537f746424dd44b55c40a07ce3f03
slimmind <slimmind@gmail.com>
2020-04-20 20:02:59 +0300

    working on note-filters

--- notix/src/js/modules/render-item.js
+++ notix/src/js/modules/render-item.js
@@ -2,7 +2,6 @@ import renderTodo from "./render-todo";
 import renderNote from "./render-note";
 
 export default function renderItem(obj) {
-    console.log("OBJ: ", obj);
     if(obj.itemType === "todo") {
         renderTodo(obj);
     } else {
