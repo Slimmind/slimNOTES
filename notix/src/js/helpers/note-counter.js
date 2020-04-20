94acb03ec4d39d821d977cd645a442e9be8a28af
slimmind <slimmind@gmail.com>
2020-04-20 20:02:59 +0300

    working on note-filters

--- notix/src/js/helpers/note-counter.js
+++ notix/src/js/helpers/note-counter.js
@@ -1,11 +1,19 @@
 export default function noteCounter() {
-    const colors = [{color: "", qty: 0}];
+    // const colorArr = [{color: "#0433ff", qty: 1}, {color: "#942192", qty: 1}];
+    const colors = [];
+    const filterTemplate = `<strong></strong>` 
 
-    for(const item of vars.tempStore) {
-        if(item.itemType === "note") {
-            
+    for (const item of vars.tempStore) {
+        if (item.itemType === "note") {
+            colors.push(item.itemColor);
         }
     }
 
-    console.log("COLORS: ", colors);
+    const colorCount = colors.reduce((total, color) => {
+        total[color] = (total[color] || 0) + 1;
+        return total;
+    }, {});
+
+
+    console.log("COLORS", colorCount);
 }
