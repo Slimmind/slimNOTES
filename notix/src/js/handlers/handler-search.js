c6aeed136127c13bf1b600f1c2871e54749cc033
slimmind <slimmind@gmail.com>
2020-04-28 22:59:05 +0300

    fixed form-toggling

--- notix/src/js/handlers/handler-search.js
+++ notix/src/js/handlers/handler-search.js
@@ -2,7 +2,7 @@ import renderItems from "../modules/render-items";
 
 export default function searchHandler(event) {
     const items = vars.tempStore.filter(item => item.itemType === vars.currentItemListType);
-    const filteredItems = items.filter(item => item.itemTitle.includes(event.target.value));
+    const filteredItems = items.filter(item => item.itemTitle.toLowerCase().includes(event.target.value.toLowerCase()));
 
     renderItems(filteredItems, vars.currentItemListType);
 }
