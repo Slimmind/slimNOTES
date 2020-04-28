4dcf4400ba891ae5ea2da076ffa0472d4a78d545
slimmind <slimmind@gmail.com>
2020-04-28 21:55:50 +0300

    fixed items rendering

--- notix/src/js/helpers/toggle-form-type.js
+++ notix/src/js/helpers/toggle-form-type.js
@@ -1,7 +1,6 @@
 import checkInput from "./check-input";
 import hide from "./hide";
 import show from "./show";
-import renderItems from "../modules/render-items"
 
 export default function toggleFormType() {
     vars.DOM.searchField.value = "";
@@ -16,15 +15,5 @@ export default function toggleFormType() {
         hide(vars.DOM.todoList, vars.DOM.todoBlock, vars.DOM.todoFiltersWrap);
         checkInput(vars.DOM.toggleList.querySelector(`[value="${vars.currentItemListType}"]`));
         checkInput(vars.DOM.toggleForm.querySelector(`[value="${vars.currentItemListType}"]`));
-    }
-
-    const items = vars.tempStore.filter(item => item.itemType === vars.currentItemListType);
-    const renderedItems = document.querySelectorAll(`.item.${vars.currentItemListType}`);
-
-    console.log(`${items.length} === ${renderedItems.length}`);
-
-    if(items.length !== renderedItems.length) {
-        console.log("=^..^=");
-        renderItems(items);
     }
 }
