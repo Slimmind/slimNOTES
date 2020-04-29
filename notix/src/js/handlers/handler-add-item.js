8fdc8cc39750a3e303b69c5246f69c6d70f77022
slimmind <slimmind@gmail.com>
2020-04-29 16:28:00 +0300

    added cleening of search field functionality

--- notix/src/js/handlers/handler-add-item.js
+++ notix/src/js/handlers/handler-add-item.js
@@ -1,9 +1,15 @@
 import toggleFormType from "../helpers/toggle-form-type";
+import renderItems from "../modules/render-items";
 
 export default function addItemHandler() {
-    console.log("TYPE: ", vars.currentItemListType);
-    toggleFormType();
-    vars.DOM.html.classList.add('no-scroll');
-    vars.DOM.itemForm.classList.add('active-window', 'create-form');
-
+    if(vars.DOM.addItemBtn.classList.contains("clear-search-field")) {
+        const items = vars.tempStore.filter(item => item.itemType === vars.currentItemListType);
+        renderItems(items, vars.currentItemListType);
+        vars.DOM.addItemBtn.classList.remove("clear-search-field");
+        vars.DOM.searchField.value = "";
+    } else {
+        toggleFormType();
+        vars.DOM.html.classList.add('no-scroll');
+        vars.DOM.itemForm.classList.add('active-window', 'create-form');
+    }
 }
--- notix/src/js/handlers/handler-search.js
+++ notix/src/js/handlers/handler-search.js
@@ -1,6 +1,11 @@
 import renderItems from "../modules/render-items";
 
 export default function searchHandler(event) {
+    if(event.target.value.length > 0) {
+        vars.DOM.addItemBtn.classList.add("clear-search-field");
+    } else {
+        vars.DOM.addItemBtn.classList.remove("clear-search-field");
+    }
     const items = vars.tempStore.filter(item => item.itemType === vars.currentItemListType);
     const filteredItems = items.filter(item => item.itemTitle.toLowerCase().includes(event.target.value.toLowerCase()));
 
