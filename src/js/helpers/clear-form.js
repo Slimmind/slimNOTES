a4732d0e3759b9443f828a32d1dcd04be7e33bfb
slimmind <slimmind@gmail.com>
2020-05-03 16:08:58 +0300

    changes for form-reset

--- src/js/helpers/clear-form.js
+++
@@ -1,6 +0,0 @@
-export default function clearForm() {
-    const forms = document.querySelectorAll("form");
-    for (const form of forms) {
-        form => form.reset();
-    }
-}
---
+++ src/js/helpers/reset-form.js
@@ -0,0 +1,3 @@
+export default function resetForm() {
+    vars.DOM.itemForm.querySelector("form").reset();
+}
